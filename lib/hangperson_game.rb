2b7216ae40968802c079821b62615f34b1df52e9
Vu Nguyen <futurus@outlook.com>
2016-06-18 15:40:44 +0000

    class HangmanGame completed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,14 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -17,6 +23,44 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(letter)
+    raise ArgumentError, 'Invalid guess' if letter.nil? || letter.empty? || (letter =~ /^[a-zA-Z]$/).nil?
+    letter.downcase!
+    
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    else  
+      if @word.include? letter
+        @guesses << letter
+      else
+        @wrong_guesses << letter
+      end
+      return true
+    end
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length == 7
+      :lose
+    elsif @word === self.word_with_guesses
+      :win
+    else
+      :play
+    end
+  end
+
+  def word_with_guesses
+    display = ''
+    @word.split('').each { |letter|
+      if @guesses.include? letter
+        display << letter
+      else
+        display << '-'
+      end
+    }
+    return display
   end
 
 end
