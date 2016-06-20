4928c332ad6d75769de040d37a5d03b5ec863bc6
Vu Nguyen <futurus@outlook.com>
2016-06-20 20:08:27 +0000

    Fix error handling and prevent cheating

--- app.rb
+++ app.rb
@@ -39,18 +39,18 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
-    
+
     begin
       status = @game.guess(letter)
-    rescue
+      
+      if !status
+        flash[:message] = "You have already used that letter."
+      end
+      
+    rescue ArgumentError
       flash[:message] = "Invalid guess."
     end
-    
-    if !status
-      flash[:message] = "You have already used that letter."
-    end
-    
+
     redirect '/show'
   end
   
@@ -60,25 +60,14 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    
+
     if @game.check_win_or_lose == :play
       erb :show # You may change/remove this line
     elsif @game.check_win_or_lose == :win
-      redirect '/win'
+      erb :win # You may change/remove this line
     else
-      redirect '/lose'
+      erb :lose # You may change/remove this line
     end
   end
-  
-  get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
-  end
-  
-  get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
-  end
-  
+
 end
